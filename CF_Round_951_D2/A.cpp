653f01b04446180241ca3235869de5bcf292337a
Ruggy Mango <rans2203@gmail.com>
2024-06-07 19:53:58 +0530

    Final code of CF 951 A

--- CF_Round_951_D2/A.cpp
+++ CF_Round_951_D2/A.cpp
@@ -1,43 +1,65 @@
 #include <iostream>
 #include <vector>
-#include <climits>
 using namespace std;
 
 int main() {
     int t;
     cin >> t;
     for (int i = 0; i < t; i++) {
-        int n, x;
+        int n;
         cin >> n;
         vector<int> arr(n);
-        int miniPrev, mini, miniNext;
         for (int j = 0; j < n; j++) {
-            cin >> x;
-            arr[j] = x;
+            cin >> arr[j];
         }
-
-        for (int j = 0; j < n; j++) {
-            if ((j > 0 & j < n-1) & arr[j] < mini )  {
-                mini = arr[j];
-                miniPrev = arr[j-1];
-                miniNext = arr[j + 1]; 
-                cout << "miniPrev1: " << miniPrev << " mini: " << mini << " miniNext: " << miniNext << endl;
-            }
-            else if (j == 0 & j < n-1) {
-                mini = arr[j];
-                miniPrev = INT_MAX;
-                miniNext = arr[j+1];
-                cout << "miniPrev2: " << miniPrev << " mini: " << mini << " miniNext: " << miniNext << endl;
-            }
-            else if (j == n-1 & arr[j] < mini) {
-                mini = arr[j];
-                miniPrev = arr[j-1];
-                miniNext = INT_MAX;
-                cout << "miniPrev3: " << miniPrev << " mini: " << mini << " miniNext: " << miniNext << endl;
-            }
+        int result = 1e9;
+        for (int j = 1; j < n; j++) {
+            result = min(result, max(arr[j - 1], arr[j]) - 1);
         }
-        int result = min(miniPrev, miniNext) - 1;
         cout << result << endl;
     }
-    return 0;
 }
+
+// #include <iostream>
+// #include <vector>
+// #include <climits>
+// using namespace std;
+
+// int main() {
+//     int t;
+//     cin >> t;
+//     for (int i = 0; i < t; i++) {
+//         int n, x;
+//         cin >> n;
+//         vector<int> arr(n);
+//         int miniPrev, mini, miniNext;
+//         for (int j = 0; j < n; j++) {
+//             cin >> x;
+//             arr[j] = x;
+//         }
+
+//         for (int j = 0; j < n; j++) {
+//             if ((j > 0 & j < n-1) & arr[j] < mini )  {
+//                 mini = arr[j];
+//                 miniPrev = arr[j-1];
+//                 miniNext = arr[j + 1];
+//                 cout << "miniPrev1: " << miniPrev << " mini: " << mini << " miniNext: " << miniNext << endl;
+//             }
+//             else if (j == 0 & j < n-1) {
+//                 mini = arr[j];
+//                 miniPrev = INT_MAX;
+//                 miniNext = arr[j+1];
+//                 cout << "miniPrev2: " << miniPrev << " mini: " << mini << " miniNext: " << miniNext << endl;
+//             }
+//             else if (j == n-1 & arr[j] < mini) {
+//                 mini = arr[j];
+//                 miniPrev = arr[j-1];
+//                 miniNext = INT_MAX;
+//                 cout << "miniPrev3: " << miniPrev << " mini: " << mini << " miniNext: " << miniNext << endl;
+//             }
+//         }
+//         int result = min(miniPrev, miniNext) - 1;
+//         cout << result << endl;
+//     }
+//     return 0;
+// }
